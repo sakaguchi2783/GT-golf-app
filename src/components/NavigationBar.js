65d1da6c7b68c25c683b471c99eb6ddeb21b4ebd
sakaguchi2783 <sakaguchi@meiko-p.co.jp>
2024-10-30 01:39:10 +0900

    UI修正８

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './NavigationBar.css';
+import './IconNavigation.css';
+
+// Importing icons
+import homeIcon from '../assets/home.jpg';
+import style4Icon from '../assets/style4.jpg';
+import biorhythmIcon from '../assets/biorhythm.jpg';
+import mypageIcon from '../assets/mypage.jpg';
 
 const NavigationBar = () => {
   return (
-    <div className="navigation-bar">
-      <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>ホーム</NavLink>
-      <NavLink to="/style4" className={({ isActive }) => (isActive ? 'active' : '')}>スタイル４診断</NavLink>
-      <NavLink to="/biorhythm" className={({ isActive }) => (isActive ? 'active' : '')}>バイオリズム診断</NavLink>
-      <NavLink to="/mypage" className={({ isActive }) => (isActive ? 'active' : '')}>マイページ</NavLink>
+    <div className="icon-navigation">
+      <NavLink to="/" className="nav-icon" activeClassName="active">
+        <img src={homeIcon} alt="ホーム" />
+      </NavLink>
+      <NavLink to="/style4" className="nav-icon" activeClassName="active">
+        <img src={style4Icon} alt="スタイル4診断" />
+      </NavLink>
+      <NavLink to="/biorhythm" className="nav-icon" activeClassName="active">
+        <img src={biorhythmIcon} alt="バイオリズム診断" />
+      </NavLink>
+      <NavLink to="/mypage" className="nav-icon" activeClassName="active">
+        <img src={mypageIcon} alt="マイページ" />
+      </NavLink>
     </div>
   );
 };
